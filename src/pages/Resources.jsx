2def64f9b1212427cac1081a5352cc74ba9faefd
soumol <cssian4@gmail.com>
2025-08-18 01:04:48 +0000

    Revert "First test chnage after git repo is created"
    This reverts commit a1b70734f039dd83bf56abfd04973fd560c4247e.

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -2,7 +2,7 @@ export default function Resources() {
   return (
     <div className="stack gap-md">
       <h1>Resources</h1>
-      <p>We’ll list reputable, medically-sound links, PDFs, research and guides here. Feel free to checkout these resources.</p>
+      <p>We’ll list reputable, medically-sound links, PDFs, and guides here.</p>
     </div>
   )
 }
